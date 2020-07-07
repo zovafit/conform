354e9ccea0a153a8ea8c7675b68e74cabc7c003d
Stuart King <stuartrexking@gmail.com>
2020-07-07 14:43:59 +1000

    Rebuild escripts for Elixir 1.10

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Conform.Mixfile do
 
     [app: :conform,
      version: "2.5.2",
-     elixir: "~> 1.3",
+     elixir: "~> 1.10",
      escript: [main_module: Conform, path: "priv/bin/conform", embed_elixir: embed_elixir?],
      compilers: compilers(@do_peg_compile?),
      build_embedded: Mix.env == :prod,
